ee03158031fde58b56720c274a293f76e62e0e35
lovy <49551140+tw-src@users.noreply.github.com>
2019-08-20 18:00:09 +0800

    Create dockerinstall.sh

---
+++ dockerinstall.sh
@@ -0,0 +1,15 @@
+# 定义安装版本
+export docker_version=18.06.3
+# step 1: 安装必要的一些系统工具
+sudo yum update -y;
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2 bash-completion;
+# Step 2: 添加软件源信息
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo;
+# Step 3: 更新并安装 Docker-CE
+sudo yum makecache all;
+version=$(yum list docker-ce.x86_64 --showduplicates | sort -r|grep ${docker_version}|awk '{print $2}');
+sudo yum -y install --setopt=obsoletes=0 docker-ce-${version} docker-ce-selinux-${version};
+# 如果已经安装高版本Docker,可进行降级安装(可选)
+yum downgrade --setopt=obsoletes=0 -y docker-ce-${version} docker-ce-selinux-${version};
+# 设置开机启动
+sudo systemctl enable docker;
